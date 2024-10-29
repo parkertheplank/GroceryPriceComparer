7d448702c48271fc2477b2832280db03c7e00ae7
parkertheplank <77869681+parkertheplank@users.noreply.github.com>
2024-10-29 16:55:42 -0600

    Update prices.js

--- prices.js
+++ prices.js
@@ -31,9 +31,13 @@ const stores = {
         url: 'https://shop.sprouts.com/landing?product_id=25446&region_id=2887106004', 
         selector: '#regular_price'
     },
+    'Safeway': { 
+        url: 'https://www.safeway.com/shop/product-details.111010341.html', 
+        selector: '.body > main > div > div > div > div > div.row.product-details-container.desktop.mt-5 > div.col-12.col-sm-12.col-md-12.col-lg-6.col-xl-6.product-details > div > div.product-details > div.product-details__product-price > div > span:nth-child(1)' 
+    },
     'Walmart': { 
         url: 'https://www.walmart.com/ip/Bear-Naked-Vanilla-Almond-Crisp-Granola-Cereal-Mega-Pack-16-5-oz-Bag/961171366', 
-        selector: '//*[@id="maincontent"]/section/main/div[2]/div[2]/div/div[3]/div/div[1]/div/div/span[1]/span[2]/span' 
+        selector: '#maincontent > section > main > div.flex.flex-column.h-100 > div:nth-child(2) > div > div.w_aoqv.w_wRee.w_b_WN > div > div:nth-child(1) > div > div > span.b.lh-copy.dark-gray.f1.mr2 > span.inline-flex.flex-column > span' 
     }
 };
 
