1592431a8c4219f3278718c492aa5065bf913f16
parkertheplank <77869681+parkertheplank@users.noreply.github.com>
2024-10-29 15:29:08 -0600

    Update selectors

--- prices.js
+++ prices.js
@@ -6,8 +6,8 @@ async function fetchPrice(url, cssSelector) {
         const page = await browser.newPage();
         await page.goto(url, { waitUntil: 'load', timeout: 0 });
 
-        // Wait for a few seconds to ensure everything is loaded
-        await page.waitForTimeout(5000);
+        // Wait for the price element to load
+        await page.waitForSelector(cssSelector, { timeout: 10000 });
 
         // Log the HTML to see the structure
         const pageHTML = await page.content();
@@ -23,8 +23,8 @@ async function fetchPrice(url, cssSelector) {
 }
 
 const stores = {
-    'Walmart': { url: 'https://www.walmart.com/ip/Bear-Naked-Vanilla-Almond-Crisp-Granola-Cereal-Mega-Pack-16-5-oz-Bag/961171366', selector: '#maincontent > section > main > div.flex.flex-column.h-100 > div:nth-child(2) > div > div.w_aoqv.w_wRee.w_b_WN > div > div:nth-child(1) > div > div > span.b.lh-copy.dark-gray.f1.mr2 > span.inline-flex.flex-column' },
-    'Safeway': { url: 'https://www.safeway.com/shop/product-details.111010341.html', selector: 'body > main > div > div > div > div > div.row.product-details-container.desktop.mt-5 > div.col-12.col-sm-12.col-md-12.col-lg-6.col-xl-6.product-details > div > div.product-details > div.product-details__product-price' },
+    'Walmart': { url: 'https://www.walmart.com/ip/Bear-Naked-Vanilla-Almond-Crisp-Granola-Cereal-Mega-Pack-16-5-oz-Bag/961171366', selector: '.price-characteristic' },
+    'Safeway': { url: 'https://www.safeway.com/shop/product-details.111010341.html', selector: '.product-price' },
     'Sprouts': { url: 'https://shop.sprouts.com/landing?product_id=25446&region_id=2887106004', selector: '.current-price' }
 };
 
@@ -34,7 +34,7 @@ const stores = {
         for (const [store, info] of Object.entries(stores)) {
             const page = await browser.newPage();
             await page.goto(info.url, { waitUntil: 'load', timeout: 0 });
-            await page.waitForTimeout(5000); // Wait for a few seconds
+            await page.waitForSelector(info.selector, { timeout: 10000 });
             const price = await page.$eval(info.selector, el => el.textContent.trim());
             console.log(`${store}: ${price}`);
             await page.close();
