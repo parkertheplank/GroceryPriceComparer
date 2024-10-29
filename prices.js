303bccefe2bad418e71d1e1688982d9d5d950ac2
parkertheplank <77869681+parkertheplank@users.noreply.github.com>
2024-10-29 15:58:38 -0600

    Update prices.js

--- prices.js
+++ prices.js
@@ -20,11 +20,11 @@ async function fetchPrice(url, cssSelector) {
 
 const stores = {
     'Sprouts': { 
-        url: 'https://shop.sprouts.com/landing?product_id=25446&region_id=2887106004', 
-        selector: '.current-price'
+        url: 'https://shop.sprouts.com/store/sprouts/products/25446-bear-naked-fit-granola-cereal-vegan-vanilla-almond-12-oz', 
+        selector: '.e-0'
     },
     'Walmart': { 
-        url: 'https://www.walmart.com/ip/Bear-Naked-Vanilla-Almond-Crisp-Granola-Cereal-Mega-Pack-16-5-oz-Bag/961171366', 
+        url: 'https://www.walmart.com/ip/Bear-Naked-Fit-Vanilla-Almond-Crisp-Granola-Cereal-12-oz-Bag/12444772?wmlspartner=wlpa&selectedSellerId=101620557&gclsrc=aw.ds&&adid=2222222222712444772_101620557_171709135088_21568604359&wl0=&wl1=g&wl2=c&wl3=709531972444&wl4=pla-2335753979702&wl5=9221267&wl6=&wl7=&wl8=&wl9=pla&wl10=5340306903&wl11=online&wl12=12444772_101620557&veh=sem&gad_source=1&gclid=Cj0KCQjwj4K5BhDYARIsAD1Ly2p-tByVDaTYnk7jrD-eu8dAOFxev3nRgw87zx7LoJeo4b3hpx9PUzAaAgH-EALw_wcB', 
         selector: '[itemprop="price"]' // Trying a more general attribute-based selector
     },
     'Safeway': { 
